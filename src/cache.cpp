420ea7986b8047012efe83b471fc69498064cc82
howeng98 <leonhwlee9@gmail.com>
2019-06-13 04:23:43 +0800

    trace1 data

--- src/cache.cpp
+++ src/cache.cpp
@@ -2,15 +2,49 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <bitset>
 
 using namespace std;
 
+unsigned get_blockAddress(unsigned block,int offset){
+	block = block >> offset;
+	return block;
+}
+
+unsigned get_tag(unsigned block,int offset,int index){
+	block = block >> (offset + index);
+	return block;
+}
+
+int calculate_index(int cache_size,int block_size){
+	int answer=0,index=0;
+	answer = cache_size/block_size;
+	while(answer != 1){
+		answer = answer / 2;
+		index++;
+	}
+	return index;
+}
+
+int calculate_wordSet(int block_size){
+	int wordSet=0;;
+	while(block_size != 1){
+		block_size = block_size / 2;
+		wordSet++;
+	}
+	return wordSet;
+}
+
 int main(){
-	vector <string> input;
-	string String;
-	int counter=0;
+	vector <unsigned> input;
+	string data;
+	int counter=0,width=32;
+	int cache_size,block_size;
+	int associativity,algorithm;
+	int tag,index,offset,index_bits;	
+
 	ifstream inFile;
-	inFile.open("trace1.txt");
+	inFile.open("./src/trace1.txt");
 
 	//Check for error
 	if(inFile.fail()){
@@ -18,16 +52,44 @@ int main(){
 		exit(1);
 	}
 
+	inFile >> cache_size >> block_size >> associativity >> algorithm;
 	while(!inFile.eof()){
-		inFile >> String;
-		input.push_back(String);
+		inFile >> data;		  
+		input.push_back(stoull(data,nullptr,0));
 		counter++;		
 	}
 
+	index_bits = calculate_index(cache_size,block_size);
+	offset = calculate_wordSet(block_size) + 2; //WO + BO
+	index = cache_size / block_size;
+	cout << "The index is " << offset << endl;
+	
 
-	for(int i=0;i<counter-1;i++)
-		cout << input[i] << endl;
-	inFile.close();
+	int cache[index] = {0};
+	
+	for(int i=0;i<counter;i++){	
+		int blockAddress = get_blockAddress(input[i],offset);
+		if(cache[blockAddress % index] != 0){
+			cout << -1 << endl;
+			cache[blockAddress % index] = get_tag(input[i],offset,index_bits);
+		}
+		else{
+			
+			cache[blockAddress % index] = get_tag(input[i],offset,index_bits);
+			cout <<  cache[blockAddress % index] << endl;
+		}
+	}
+	
+
+	// //Print out for checking
+    // cout << cache_size << endl << block_size << endl << associativity << endl << algorithm << endl;
+	// for(int i=0;i<counter;i++){
+	// 	cout << std::bitset<32>(input[i]) << endl;
+	// }	
+	
+	inFile.close();	
 	return 0;	
 
 }
+
+
