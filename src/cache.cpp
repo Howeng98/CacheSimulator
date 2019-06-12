c3574439b964b9cfb9edb9618e007158344722e1
howeng98 <leonhwlee9@gmail.com>
2019-06-13 00:52:23 +0800

    input data

--- src/cache.cpp
+++ src/cache.cpp
@@ -1,11 +1,33 @@
-//this is cache.cpp
-#include <stdio.h>
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
 int main(){
-//	cout << "This is cache.cpp" << endl;
+	vector <string> input;
+	string String;
+	int counter=0;
+	ifstream inFile;
+	inFile.open("trace1.txt");
+
+	//Check for error
+	if(inFile.fail()){
+		cout << "Error Opening File" << endl;
+		exit(1);
+	}
+
+	while(!inFile.eof()){
+		inFile >> String;
+		input.push_back(String);
+		counter++;		
+	}
+
+
+	for(int i=0;i<counter-1;i++)
+		cout << input[i] << endl;
+	inFile.close();
 	return 0;	
 
 }
