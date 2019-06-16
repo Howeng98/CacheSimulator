33278ab0be4b6e112aea1f3658c85e842df528f7
howeng98 <leonhwlee9@gmail.com>
2019-06-17 00:37:04 +0800

    Finish!

--- src/cache.cpp
+++ src/cache.cpp
@@ -41,48 +41,35 @@ int calculate_wordSet(int block_size){
 }
 
 int printResult(vector<int> result,int size){
-	for(int i=0;i<size;i++){
-		//if(result[i] != -1)
-		//	cout << result[i] << endl;		
+	for(int i=0;i<size;i++){	
 		if(i != size -1)
 			outFile << result[i] << endl;
 		else
 			outFile << result[i];				
 	}
 }
-/* 
-bool isFull(vector<unsigned> cache,int blockNumber,int tagValue){
-	for(int i=0;i<blockNumber;i++){
-		if(cache[0][i] == 0){			
-			return false;
-		}
-	}
-	return true;
-}
 
-bool isHit(vector<unsigned>cache,int blockNumber,int tagValue){
-	
-}
-*/
 int main(int argc,char *argv[]){
 	vector <unsigned> input;
 	string data;
-	int counter=0,width=32,checking_counter=0;	
-	int x,largest = 0;
+	int counter=0,largest=0;	
 	int cache_size,block_size;
 	int associativity,algorithm;
 	int tag,index,offset,index_bits;
 	int blockAddress,tagValue,blockNum;	
 	
+	//Read and Write data
 	inFile.open(argv[1]); 
 	outFile.open(argv[2]);
+
 	//Check for error
 	if(inFile.fail()){
 		cout << "Error Opening File" << endl;
 		exit(1);
 	}
-	
+
 	inFile >> cache_size >> block_size >> associativity >> algorithm;
+	//Cache size is Kb,need to * 1024
 	cache_size = cache_size * 1024;	
 	while(!inFile.eof()){
 		inFile >> data;		  
@@ -90,25 +77,28 @@ int main(int argc,char *argv[]){
 		counter++;		
 	}
 	//Initial
-	if(associativity == 0){ //directedMapped
+	//directedMapped
+	if(associativity == 0){ 
 		index_bits = calculate_index(cache_size,block_size);
 		offset = calculate_wordSet(block_size);
 		index = cache_size / block_size;
 		tag = 32 - offset - index_bits;
 	}
-	else if(associativity == 1){ //4-Way
+	//4-Way
+	else if(associativity == 1){ 
 		index_bits = calculate_index(cache_size,(block_size*4));
 		offset = calculate_wordSet(block_size);
 		index = cache_size / (block_size*4);
 		tag = 32 - offset - index_bits;
 	}
+	//Fully
 	else if(associativity == 2){
 		index_bits = 0; //2^0 = 1(1set)
 		offset = calculate_wordSet(block_size);
 		index = 2^(index_bits);
 		tag = 32 - offset - index_bits;
 	}
-	
+	/*  For checking 
 	cout << "Cache_Size is : " << cache_size << endl;
 	cout << "Block_Size is : " << block_size << endl;
 	cout << "index is : " << index << endl;
@@ -117,9 +107,11 @@ int main(int argc,char *argv[]){
 	cout << "tag_bits is : " << tag << endl;
 	cout << "Associative is : " << associativity << endl;
 	cout << "Algorithm is : " << algorithm << endl;
+	*/
 	vector <unsigned> cache [index];	
 	
-	if(associativity == 0 && (algorithm == 0 || algorithm == 1 || algorithm == 2)){ //directedMapped_FIFO		
+	//directedMapped
+	if(associativity == 0){ 	
 			for(int i=0;i<index;i++){		
 				cache[i].push_back(0);
 			}		
@@ -146,9 +138,11 @@ int main(int argc,char *argv[]){
 				exit_direct_FIFO:;	
 			}
 		printResult(result,result.size());				
-	} //end directedMapped_FIFO	
+	} //end directedMapped
 	
-	if(associativity == 1){ //4-way	
+	//4-way
+	if(associativity == 1){ 	
+		//Initialize cache
 		for(int i=0;i<index;i++){
 			cache[i].push_back(0);cache[i].push_back(0);cache[i].push_back(0);cache[i].push_back(0);			
 		}
@@ -157,73 +151,82 @@ int main(int argc,char *argv[]){
 			blockAddress = get_blockAddress(input[i],offset);
 			tagValue = get_tag(input[i],offset,index_bits);
 			blockNum = blockAddress % index;			 
-			for(int columnNumber = 0;columnNumber < 4;columnNumber++){
+			//Check 4 column
+			for(int columnNumber = 0;columnNumber < 4;columnNumber++){ 
+				//Hit
 				if(tagValue == cache[blockNum][columnNumber]){
 					result.push_back(-1);
 					if(algorithm != 0){ //Only LRI need to reset position, otherwise FIFO no need
 						cache[blockNum].erase(cache[blockNum].begin()+columnNumber);							
 						cache[blockNum].push_back(tagValue);
 					}
-					goto exit_4way_FIFO;
+					goto exit_4Way;
 				}
 			}
+			
 			for(int columnNumber = 0;columnNumber < 4;columnNumber++){				
+				//Miss_notExist
 				if(cache[blockNum][columnNumber] == 0){
 					result.push_back(-1);
 					cache[blockNum][columnNumber] = tagValue;
-					goto exit_4way_FIFO;
+					goto exit_4Way;
 				}				
 			}
-			{				
+			{		
+				//Miss_Exist		
 				result.push_back(cache[blockNum][0]);				
 				cache[blockNum].erase(cache[blockNum].begin());
 				cache[blockNum].push_back(tagValue);																				
 			}
 			
-			exit_4way_FIFO:;
+			exit_4Way:;
 		}		
 		printResult(result,result.size());
-	} //end 4way_FIFO
+	} //end 4-way
 	
-	if(associativity == 2){ //Fully
+	//Fully
+	if(associativity == 2){ 
 			int blockNumber = cache_size / block_size;			
+			//initialize cache
 			for(int i=0;i<blockNumber;i++)
-				cache[0].push_back(0); //initialize cache						
+				cache[0].push_back(0); 						
 			for(int i=0;i<counter;i++){							
 				tagValue = get_tag(input[i],offset,index_bits);								
 				for(int columnNumber = 0;columnNumber < blockNumber;columnNumber++){
+					//Hit
 					if(tagValue == cache[0][columnNumber]){
 						result.push_back(-1);
 						if(algorithm != 0){
 							cache[0].erase(cache[0].begin()+columnNumber);							
 							cache[0].push_back(tagValue);
 						}
-						goto exit_LRU;
+						goto exit_Fully;
 					}
 				}
 				
 				for(int columnNumber = 0;columnNumber < blockNumber;columnNumber++){				
+					//Miss_notExist
 					if(cache[0][columnNumber] == 0){
 						result.push_back(-1);
 						cache[0][columnNumber] = tagValue;
-						goto exit_LRU;
+						goto exit_Fully;
 					}				
 				}				
 				
-				{			
+				{	
+					//Miss_Exist
 					result.push_back(cache[0][0]);				
 					cache[0].erase(cache[0].begin());
 					cache[0].push_back(tagValue);							
 				}																										
 				
-				exit_LRU:;
+				exit_Fully:;
 			}
 			printResult(result,result.size());
-	}	//end Fully_LRU	
+	}	//end Fully
 			
 	inFile.close();	
-	outFile.close();
-	cout << "Finish" << endl;
+	outFile.close();	
 	return 0;	
 }
 
